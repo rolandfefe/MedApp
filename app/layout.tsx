3aff18b8d6cd52c5144a8c8a113ff2113461d9a5
Code-medic-official <codemedic2@gmail.com>
2025-08-30 14:18:04 +0300

    Initialted project will all major tool configurations

---
+++ app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "@/app/api/uploadthing/core";
+
+const geistSans = Geist({
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: "Health App",
+	description: "Saving Live is all we do.",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
+				<NextSSRPlugin
+					/**
+					 * The `extractRouterConfig` will extract **only** the route configs
+					 * from the router to prevent additional information from being
+					 * leaked to the client. The data passed to the client is the same
+					 * as if you were to fetch `/api/uploadthing` directly.
+					 */
+					routerConfig={extractRouterConfig(ourFileRouter)}
+				/>
+				{children}
+			</body>
+		</html>
+	);
+}
